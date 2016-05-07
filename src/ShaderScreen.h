83080b1e35bf7da24be0b8a61d4ac5f42ebe1c29
Andy Dayton <amdayton@gmail.com>
2016-05-06 23:02:42 -0400

    Cleanup shader screen

--- src/ShaderScreen.h
+++ src/ShaderScreen.h
@@ -16,18 +16,9 @@ class ShaderScreen {
     ofMesh mesh;
     
     void setup(int w, int h, bool arb= true) {
-      // set texture coordinates for arbitrary
-      // or non-arbitrary texture coords
-      int tw = arb ? w : 1;
-      int th = arb ? h : 1;
     
+      // set gl mode
       mesh.setMode(OF_PRIMITIVE_TRIANGLE_FAN);
-      
-      // add vertices
-      mesh.addVertex(ofVec3f(0, 0, 0));
-      mesh.addVertex(ofVec3f(w, 0, 0));
-      mesh.addVertex(ofVec3f(w, h, 0));
-      mesh.addVertex(ofVec3f(0, h, 0));
       
       // add indexes
       mesh.addIndex(0);
@@ -35,16 +26,33 @@ class ShaderScreen {
       mesh.addIndex(2);
       mesh.addIndex(3);
       
+      // set vertices and tex coordinates
+      update(w, h, arb);
+
+    };
+  
+    void update(int w, int h, bool arb=true) {
+      // set texture coordinates for arbitrary
+      // or non-arbitrary texture coords
+      int tw = arb ? w : 1;
+      int th = arb ? h : 1;
+
+      // reset
+      mesh.clearVertices();
+      mesh.clearTexCoords();
+
+      // add vertices
+      mesh.addVertex(ofVec3f(0, 0, 0));
+      mesh.addVertex(ofVec3f(w, 0, 0));
+      mesh.addVertex(ofVec3f(w, h, 0));
+      mesh.addVertex(ofVec3f(0, h, 0));
+      
       // add texture coordinates
       mesh.addTexCoord(ofVec2f(0, 0));
       mesh.addTexCoord(ofVec2f(tw, 0));
       mesh.addTexCoord(ofVec2f(tw, th));
       mesh.addTexCoord(ofVec2f(0, th));
-    };
-  
-//    void update(int w, int h) {
-//      // update mesh size
-//    }
+    }
   
     void draw() {
       mesh.draw();
